8f46564fcaf63abdfb04b0f450fdc8f1fbfb7461
Dev Jana <devjana@Devs-MBP-2.hsd1.mn.comcast.net>
2022-06-15 15:07:15 -0500

    end of lecture 0

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # React View Routing
+===
+
+- make sure 'react-router-dom' is in dependencies, if not, npm install it
+- in App.jsx (or any component that will use client side routing): ```import { Route, HashRouter } from 'react-router-dom';```
+- set up your routes:
+
+```<HashRouter>
+    <Route path="/" exact>
+        <Home />
+    </Route>
+    <Route path="/animals">
+        <Animals />
+    </Route>
+    <Route path="/plants">
+        <Plants />
+    </Route>
+</HashRouter>```
+
+- set up links as usual for nav bar:
+
+```      
+<ul>
+    <li><a href="/">Home</a></li>
+    <li><a href="/#/animals">Animals</a></li>
+    <li><a href="/#/plants">Plants</a></li>
+</ul>
+```
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Route, HashRouter as Router} from 'react-router-dom';
 
 import Animals from '../Animals/Animals.jsx';
 import Plants from '../Plants/Plants.jsx';
@@ -9,6 +10,27 @@ function App() {
   return (
     <div className="App">
       <h1>SPAs!</h1>
+      <p>Dev's stuff</p>
+
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/#/plants">Plants</a></li>
+        <li><a href="/#/animals">Animals</a></li>
+      </ul>
+
+      <Router>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/animals">
+          <Animals />
+        </Route>
+        <Route path="/plants">
+          <Plants />
+        </Route>
+      </Router>
+      
+      <h2>Footer goes here</h2>
     </div>
   );
 }
